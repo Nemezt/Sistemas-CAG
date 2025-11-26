00384c9d1cd7502829e7e687f16c68370cba511f
Nemezt <113740944+Nemezt@users.noreply.github.com>
2025-11-25 21:01:50 -0600

    Commit

--- Sistemas CAG/obj/Debug/net8.0-windows/Lanzador de Sistemas.AssemblyInfo.cs
+++ Sistemas CAG/obj/Debug/net8.0-windows/Lanzador de Sistemas.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2025 CoopeAgri R.L. Todos los derechos reservados.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Aplicación lanzadora de sistemas, actualiza los sistemas antes de ejecutarlos.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7072f4c4016ccf4791457527defd71908a3f5c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b10e72194f9ae18ad5321127d6d154a78604855")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lanzador de Sistemas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lanzador de Sistemas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("5.0.0.0")]
