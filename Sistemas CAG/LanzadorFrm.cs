b4f28764ff81b5614ce824990d1874ed80cb9ceb
YARIAS <yarias@coopeagri.co.cr>
2026-05-19 13:25:34 -0600

    Correcciones

--- Sistemas CAG/LanzadorFrm.cs
+++ Sistemas CAG/LanzadorFrm.cs
@@ -250,7 +250,7 @@ namespace Sistemas_CAG
                     {
                         fLPDesktop.Controls.Add(contenedor);
                     }
-                    else if (btn.Tipo == "web" || btn.Tipo == "Javaws")
+                    else if (btn.Tipo == "web" || btn.Tipo == "javaws")
                     {
                         fLPWeb.Controls.Add(contenedor);
                     }
--- Sistemas CAG/obj/Release/net8.0-windows/Lanzador de Sistemas.AssemblyInfo.cs
+++ Sistemas CAG/obj/Release/net8.0-windows/Lanzador de Sistemas.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("© 2025 CoopeAgri R.L. Todos los derechos reservados.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Aplicación lanzadora de sistemas, actualiza los sistemas antes de ejecutarlos.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364b89a2ea811456e985c19f1f80ecad78ab3eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb4848a30829449391e88aecf9f8d8c33a8983c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lanzador de Sistemas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lanzador de Sistemas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("5.0.0.0")]
